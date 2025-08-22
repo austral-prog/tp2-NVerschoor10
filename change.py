633f6d9f89cad0e6bed121f716da5bc5a362b206
NVerschoor10 <nverschoor@mail.austral.edu.ar>
2025-08-22 00:03:13 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,21 @@
 def change():
     expense = 23.75
     money = 100
+
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    p = money - expense
+    p = int(p)
+    b = (money - expense)
+    c = b-p
+    c = c*100
+    c = int(c)
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print(p)
+    print("Centavos:")
+    print(c)
